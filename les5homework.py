03e78aa4c10f1c4718728451ebddeb01eac5c828
Best Cat <kotova-irina11@rambler.ru>
2022-10-31 20:42:59 +0300

    les5 homework

--- les5homework.py
+++ les5homework.py
@@ -16,3 +16,11 @@ print(x, x*4, x)
 # y = 5         5 = z - 7   z = 12
 x, y, z = map(int, input().split())
 print(x*3 + y*5 + z*12)
+# ЭНИЯ  https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=1&id_topic=26&id_problem=145
+# n=кол-во панелей a и b = длина и ширина *2 стороны панели
+n, a ,b = map(int, input().split())
+print(n*a*b*2)
+# БАНДИТЫ https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=1&id_topic=26&id_problem=146
+a, b = map(int, input().split())
+s = a + b - 1
+print(s - a, s - b)
