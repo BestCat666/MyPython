fa59c575a87b9e48decc5644990e5da81a9dfebd
Best Cat <kotova-irina11@rambler.ru>
2022-10-31 17:10:03 +0300

    les5 input

---
+++ les5.py
@@ -0,0 +1,8 @@
+a = input()
+print(a + a)            # a = str
+a = int(a)              # чтобы преобразовать а в число
+print(a + a)
+b = int(input())
+print(b + b)            # b = целое число
+c = float(input())      # c = вещественное число
+print(c + c)
