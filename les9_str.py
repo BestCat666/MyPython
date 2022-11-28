5f3c5ae6ed50807b5b9a1ca402bbf6f339b84aac
Best Cat <kotova-irina11@rambler.ru>
2022-11-28 18:15:55 +0300

    str что можно делать со строками

---
+++ les9_str.py
@@ -0,0 +1,17 @@
+# строка - это упорядоченная коллекция символов, это неизменяемый объект!
+print('hello')
+print("hello")
+print('hello\nworld') # \n перенос на новую строчку
+s = 'hello'
+#r = input()
+#print(r)
+print('ghb' + 'sdks') # сложение строк (конкатенация)
+print('a' * 10) # умножение строк
+print('ghb' + str(3))
+print(len('ansnsnns'))
+print('h' in s) # наличие символа в строке
+print('aaa' == 'aaa') # сравнение строк, важен регистр
+print('abc' > 'r') # сравнение по символьно!! сравниваются коды
+print(ord('a')) # получение кода символа ascii code table
+
+
